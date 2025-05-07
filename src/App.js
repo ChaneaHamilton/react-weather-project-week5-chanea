5f89963cddfd9dc4e37e7aa61b264f1abcd9cbfe
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-07 09:52:08 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,33 +3,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>New York</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/ChaneaHamilton"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Chanea Hamilton
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/ChaneaHamilton/react-weather-project-week5-chanea"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://chanea-weather-react-project.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div classname="container">
+        <h1>New York</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/ChaneaHamilton"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Chanea Hamilton
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ChaneaHamilton/react-weather-project-week5-chanea"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://chanea-weather-react-project.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
