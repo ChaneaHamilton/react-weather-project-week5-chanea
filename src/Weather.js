a17627c489056ee3b8557d7f80e812134c9f3cd4
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-07 10:55:04 -0600

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -6,15 +6,20 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div clasName="col-8">
+          <div className="col-9">
             <input
               text="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
-          <div className="col-4">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,11 +30,16 @@ export default function Weather() {
       </ul>
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="sunny"
-          />
-          15°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="sunny"
+              className="float-left"
+            />
+
+            <span className="temperature">15</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
