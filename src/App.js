c3cf630428cbaa3fb146df3ca044eba306cc3c80
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-07 09:44:22 -0600

    added links to github and netlify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ export default function App() {
   return (
     <div className="App">
       <h1>New York</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/ChaneaHamilton"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Chanea Hamilton
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ChaneaHamilton/react-weather-project-week5-chanea"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://chanea-weather-react-project.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
